7f919b50d9510418fca93fadd5e1de5bf8cb1152
alan.geirnaert <alan.geirnaert@icloud.com>
2025-04-06 12:18:45 +0000

    feat: added Analytics from Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import React from "react"
+
+import { Analytics } from "@vercel/analytics/react"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased max-sm:px-8`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
